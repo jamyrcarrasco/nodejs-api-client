ab218782f8b9a19330e03fb03d6112e4bce1077d
Jamyr Carrasco F <33583213+jamyrcarrasco@users.noreply.github.com>
2022-07-28 23:28:08 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+///----------IMPORT ROUTES---------
+const authRoute = require("./routes/auth");
+
+////--------CONNECT TO DB--------
+mongoose.connect(process.env.DB_CONNECTION, { useUnifiedTopology: true }, () =>
+  console.log("Connected to DB⚡")
+);
+
+///----MIDDLEWARE
+app.use(express.json());
+
+////---------ROUTES MIDDLEWARES-------
+app.use("/api/user", authRoute);
+
+app.use((req, rest, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is Up and Running⚡ on port ${process.env.PORT}`);
+});
